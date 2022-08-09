255a6e021e087f06a0ad74eae3df6f6211564614
br5kly <108040523+br5kly@users.noreply.github.com>
2022-08-10 01:11:33 +0200

    برمجه زياد العباني

---
+++ ID-2004.py
@@ -0,0 +1,192 @@
+import os
+try:
+        import rich
+except ImportError:
+        os.system('pip install rich')
+import base64
+import uuid
+import random
+import os
+import time
+import requests
+import sys
+os.system('clear')
+def smsm():
+ smsm = "\033[1;36;40m ACTIVE ID  [BR3K]\n10% [BR3K] \n[BR3K] \n[BR3K] \n[90%] \n \033[3;36;46m [%100 DONE] "
+ for letter in smsm:
+  time.sleep(0.03)
+  sys.stdout.write(letter)
+  sys.stdout.flush()
+  
+def logo():
+  log = " \033[1;36;40m WELCOME TO MY TOOL [ZEYAD ALABANY]\n \033[1;32;42m cheking system ...\n[DONE100%] \n \033[1;31;41m  ﻞﻌﻔﻣ ﺮﻴﻏ "
+  for letter in log:
+   time.sleep(0.04)
+   sys.stdout.write(letter)
+   sys.stdout.flush()
+def clear():
+  os.system('clear')
+# BACK
+def back():
+  login()
+
+ahsan="BR3K-"
+imt="-M4786=="
+ak="ZEYAD-"
+myid=uuid.uuid4().hex[:10].upper()
+try:
+  key1 = open('/data/data/com.termux/files/usr/bin/.mrahsan-cov', 'r').read()
+except:
+  kok=open('/data/data/com.termux/files/usr/bin/.mrahsan-cov', 'w')
+  kok.write(myid+imt)
+  kok.close()
+def unsubc():
+ from rich.console import Console
+ from rich.table import Table
+ table = Table(title="Zeyad alabany",style="cyan")
+ table.add_column("TOOL" , justify="right" , style="cyan")
+ table.add_column("YOUR Key" , style="magenta")
+ table.add_column("KEY", justify="right", style="blue")
+ table.add_row(" \n [VIP]\n",ak+ahsan+key1)
+ table.add_row("", " ")
+
+
+ console = Console()
+ console.print(table)
+def clear():
+  os.system('clear')
+def Subscraption():
+  key1=open('/data/data/com.termux/files/usr/bin/.mrahsan-cov', 'r').read()
+  clear()
+  
+  r1=requests.get("https://pastebin.com/LXxCq7SG").text
+  if key1 in r1:
+    os.system('clear')
+    smsm()
+    print("\033[0;30;40m welcome")
+    time.sleep(2)
+    pass
+  
+  else:
+    os.system("clear")
+    logo()
+    print("\033[0;30;40m welcome")
+    unsubc()
+    
+    print("\t \033[1;32m First Get Approvel\033[1;37m ")
+    time.sleep(1)
+    
+    
+    print ("")
+    print(" \033[1;34;40m BR3K Tool Paid You Need Get Approved First\033[1;37m\n")
+    print("")
+    print(" \033[1;34;40mCopy And Send Key To Admin")
+    name = input("\033[1;34;40m Type Your Name : ")
+    print ("")
+    input("\033[1;34;40m Press Enter To Send Key Admin Whatsapp")
+    time.sleep(3.5)
+    tks = 'Dear%20Admin,%20Please%20Approved%20My%20Key%20To%20Premium%20%20Thanks%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20Name%20:%20'+name+'%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20%20Key%20%20:%20'+ak+ahsan+''+key1
+    os.system('am start https://wa.me/+218910911646?text=' + tks)
+    Subscraption()        
+Subscraption()  
+
+from rich.console import Console
+from rich.table import Table
+os.system('clear')
+table = Table(title="\033[1;34;40m Zeyad \033[1;34;40m ALABANY")
+
+table.add_column("ﺞﻣﺮﺒﻤﻟﺍ", style="red")
+table.add_column("ID GEN 2022", style="red")
+table.add_column("ﻩﺍﺩﻻﺍ ﺮﻌﺳ", justify="right" , style="blue")
+table.add_row("Z3Y4D ALABANY" , "ID-GEN 2022 THIS TOOL GEN IDS (2004-05-06-07) " , "25 LIBYANA[VIP]", style="purple")
+console = Console()
+console.print(table)
+
+def xx():
+  start = int(input("\n[TYPE FIRST ID]:  "))
+  end = int(input("\n[TYPE SECONDE ID]:  "))
+  while start < end:
+   start += 1
+   print ( start)
+   
+def random():
+  
+  from random import randint
+  x = int(input("\033[1;34;40m \n[choose]:  "))
+  y = "https://www.facebook.com/profile.php?id="
+  for i in range(x):
+   print("\033[1;34;40m "+ y ,randint(568000000,568999999),sep="")
+   	
+
+def rand():
+  from random import randint
+  
+  vs = int(input("\n[WHAT DO WANT beginning ID ?] [2] , [7] , [5]:  "))
+  url = "https://www.facebook.com/profile.php?id="
+  tt = 60000000
+  vv = 69999999
+  for i in range(200):
+    print(url , vs ,randint(60000000,69999999),sep="")
+def magic():
+ from random import randint
+ sxtz1 = int(input("choose 10, 11 , 12")) 
+ sxtz2 = "https://www.facebook.com/profile.php?id="
+ sxtz3 = 000000
+ sxtz4 = 999999
+ for i in range(100):
+   print(sxtz2,sxtz1,randint(000000,999999),sep="")
+
+
+mrx = "\033[1;32;40m JOIN US TELE @t.me/srtly\n [BR3K-VIP] " 
+def type(mrx):
+  for char in mrx:
+   sys.stdout.write(char)
+   sys.stdout.flush()
+   time.sleep(0.04)
+def checker():
+  time.sleep(1)
+  os.system('xdg-open https://checker.fb.rip/app/')
+note = "\033[1;37;40m My Facebook [Zeyad Alabany] \n MY TELE @sxtz0"
+def note(): 
+  string="\033[1;31;40m My Facebook [Zeyad Alabany] \n MY TELE @sxtz0\n"
+  for letter in string:
+   time.sleep(0.04) # In seconds
+   sys.stdout.write(letter)
+   sys.stdout.flush()
+def menu():
+  menu()   
+from rich.console import Console
+console = Console()
+console.print("           [01] Manual", style="bold underline red")
+console.print("           [02]  2007-2006 Get Random ID",style="bold underline red")
+console.print("           [03] 2005-2006 (200) ID Random",style="bold underline red")
+console.print("           [04]2004 (200) ID Random [VIP]",style="cyan underline green ")
+console.print("           [5] MY @FACEBOOK MY TELE [BR3K]",style="blue underline blue ")
+zeyad =input("\033[1;34;40m [choose]: ")
+if zeyad in ["01" , "1"]:
+   type(mrx)
+   xx()
+   input("PRESS TO OPEN CHECKER")
+   checker()
+if zeyad in ["02" , "2"]:
+   type(mrx)
+   random()
+   input("PRESS TO OPEN CHEKCER!")
+   checker()
+if zeyad in ["03" , "3"]:
+   type(mrx)
+   rand()
+   input("PRESS TO OPEN CHECKER")
+   checker()
+if zeyad in ["04" , "4"]:
+   type(mrx)
+   magic()
+   input("PRESS TO OPEN CHECKER")
+   checker()
+if zeyad in ["05", "5"]:
+   note()
+   sys.exit() 
+else:
+   print("\033[1;36;40m ZEYAD ALABANY ")
+   time.sleep(1)
+   os.system('python ID-2004.py')
