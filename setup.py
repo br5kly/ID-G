2a476ebb8d165e122ba41e4c8a58a8a85baf9402
br5kly <108040523+br5kly@users.noreply.github.com>
2022-11-21 19:41:56 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+import os
+os.system('pip install rich')
+os.system('pip install bs4')
+os.system('pip install colorama')
+os.system('pip install cython')
+os.system('pip install stdiomask')
+os.system('pip install lisensing')
