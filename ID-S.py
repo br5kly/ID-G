a763556b68d7b5e1d608dffe9fc0ae223b8ef20d
br5kly <108040523+br5kly@users.noreply.github.com>
2022-11-21 18:39:55 +0200

    Update ID-S.py

--- ID-S.py
+++ ID-S.py
@@ -2,6 +2,6 @@ import platform
 import os
 bit = platform.architecture()[0]
 if bit == "64bit":
-   os.system('cd 64B && python ID-S.py')
+   os.system('cd B64 && python ID-S.py')
 if bit == "32bit":
-    os.system('cd 32B && python ID-S.py')
+    os.system('cd B32 && python ID-S.py')
