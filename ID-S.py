89e4a2dcc169c495accd52cd4aa2610576146c1b
br5kly <108040523+br5kly@users.noreply.github.com>
2022-11-21 15:21:21 +0200

    Update ID-S.py

--- ID-S.py
+++ ID-S.py
@@ -1,2 +1,8 @@
-from ssz import sql
-sql()
+import platform
+bit = platform.architecture())[0]
+if bit == "64bit":
+   from srq import sql
+   sql()
+if bit == "32bit":
+    from ssz import sql 
+    sql()
