ca62fceb28049a80dae894fe8f964d792895da03
br5kly <108040523+br5kly@users.noreply.github.com>
2022-12-16 03:33:44 +0200

    Update ID-S.py

--- ID-S.py
+++ ID-S.py
@@ -1,5 +1,9 @@
 import platform
 import os
+try:
+    import requests
+except ModuleNotFoundError:
+    os.system("python setup.py")
 bit = platform.architecture()[0]
 if bit == "64bit":
    os.system('cd as-main && python s.py')
